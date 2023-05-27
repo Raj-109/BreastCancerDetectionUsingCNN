331c53196cab53f80efb3b3f3ad6929143d20b79
rajat <rajatagarwal.109@gmail.com>
2023-05-27 23:55:19 +0530

    changes

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import os
 import uuid
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/', methods=['GET'])
 def hello():
     return "API SUPPORT FOR BREAST_CANCER_DETECTION_USING_CNN"
 
+@app.route('/Saveuser',methods=['POST'])
+def saveUser():
+    return "User Details Saved"
 
 @app.route('/predict', methods=['POST'])
+@cross_origin()
 def predict():
     from BreastCancer import predictImage  # Import predictImage function here
     if 'image' not in request.files:
